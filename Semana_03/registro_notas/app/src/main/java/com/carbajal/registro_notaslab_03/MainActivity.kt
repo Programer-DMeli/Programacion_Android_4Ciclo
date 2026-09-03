fce7cae8e3d04c8aa740f0499d3783cd1ae4b82d
Programer-DMeli <carbajalmeli369@gmail.com>
2026-09-03 09:05:40 -0500

    Agrege Encabezado principal con fondo degradado suave

--- Semana_03/registro_notas/app/src/main/java/com/carbajal/registro_notaslab_03/MainActivity.kt
+++ Semana_03/registro_notas/app/src/main/java/com/carbajal/registro_notaslab_03/MainActivity.kt
@@ -4,14 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableDoubleStateOf
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.tooling.preview.Preview
 import com.carbajal.registro_notaslab_03.ui.theme.Registro_notasLab_03Theme
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,6 +46,46 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun visualizacionPantalla(modifier: Modifier = Modifier){
     //Listas de almacenamiento de las 4 notas
+    var nota1 by remember { mutableFloatStateOf(0f) }
+    var nota2 by remember { mutableFloatStateOf(0f) }
+    var nota3 by remember { mutableFloatStateOf(0f) }
+    var nota4 by remember { mutableFloatStateOf(0f) }
+
+    //variables para los controles switch y checkbox
+    var redondear by remember { mutableStateOf(false) }
+    var confirmado by remember { mutableStateOf(false) }
+
+    //Para observar el resultado
+    var promedioPonderado by remember { mutableDoubleStateOf(0.0) }
+    var promedioFinalStr by remember { mutableStateOf("") }
+    var observacion by remember { mutableStateOf("") }
+    var chipColor by remember { mutableStateOf(Color.Gray) }
+    var mostrarResultado by remember { mutableStateOf(false) }
+    //implementacion de color para fondo degradado suave
+
+    val purplePrimary = Color(0xFF5E4B8B)
+    val purpleLightBackground = Color(0xFFEDE7F6)
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(purplePrimary, purpleLightBackground)
+                )
+            )
+    ) {
+
+        //Encabezado principal
+        Box(
+            modifier = Modifier.fillMaxWidth().padding(vertical =  16.dp, horizontal = 20.dp))
+        {
+            Text(text = "Registro de Notas", color = Color.White,
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold
+
+            )
+        }
+    }
 
 
 }
