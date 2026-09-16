40b05ce3df611f46c7fbfaf5da8aec7d2d9e24cc
Programer-DMeli <carbajalmeli369@gmail.com>
2026-09-16 16:44:52 -0500

    Avance: intermedio de LazyColumn

---
+++ Semana04/Carrito-S04/app/src/main/java/com/lab04_carrito_carbajal/PantallaCarrito.kt
@@ -0,0 +1,35 @@
+package com.lab04_carrito_carbajal
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import java.util.Locale
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PantallaCarrito() {
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxWidth()
+            .weight(1f),
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        items(productos) { producto ->
+            TarjetaProducto(
+                producto = producto,
+                onEliminar = { productos.remove(producto) }
+            )
+        }
+    }
+}
