87cabe7bd42fac23b51075f81d2d5fc9a4ca6fc8
Programer-DMeli <carbajalmeli369@gmail.com>
2026-09-23 11:20:05 -0500

    feat: Se agrego la funcionalidad como pantalla de navegacion la opcion de ver HistorialMedica de cada medico

--- Semana05/ClinicaSalud/app/src/main/java/com/carbajal/appclinica/screens/MedicalHistory.kt
+++ Semana05/ClinicaSalud/app/src/main/java/com/carbajal/appclinica/screens/MedicalHistory.kt
@@ -1,2 +1,44 @@
 package com.carbajal.appclinica.screens
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.carbajal.appclinica.ui.theme.SaludTextSecondary
+import com.clinicasalud.app.ui.theme.SaludTextSecondary
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MedicalHistoryScreen(onOpenDrawer: () -> Unit) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text("Historial médico", fontWeight = FontWeight.Bold) },
+                navigationIcon = {
+                    IconButton(onClick = onOpenDrawer) {
+                        Icon(Icons.Default.Menu, contentDescription = "Menú Lateral")
+                    }
+                }
+            )
+        }
+    ) { paddingValues ->
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = "Sin registros recientes en tu historial",
+                fontSize = 15.sp,
+                color = SaludTextSecondary
+            )
+        }
+    }
+}
